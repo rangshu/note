90cb13d6cdde2635b68bbcebcfb6ad39b3284cba
soorink <soorink@gmail.com>
2023-03-19 17:35:45 +0900

    Update NoteApp for use relam

--- Note/NoteApp.swift
+++ Note/NoteApp.swift
@@ -6,12 +6,35 @@
 //
 
 import SwiftUI
+import RealmSwift
+
+let theAppConfig = loadAppConfig()
+
+let app = App(id: theAppConfig.appId, configuration: AppConfiguration(baseURL: theAppConfig.baseUrl, transport: nil, localAppName: nil, localAppVersion: nil))
 
 @main
-struct NoteApp: App {
+struct NoteApp: SwiftUI.App {
+    @StateObject var errorHandler = ErrorHandler(app: app)
+    
     var body: some Scene {
         WindowGroup {
-            MenuListView()
+            MenuListView(app: app)
+                .environmentObject(errorHandler)
+                .alert(Text("Error"), isPresented: .constant(errorHandler.error != nil)) {
+                    Button("OK", role: .cancel) { errorHandler.error = nil }
+                } message: {
+                    Text(errorHandler.error?.localizedDescription ?? "")
+                }
+        }
+    }
+}
+
+final class ErrorHandler: ObservableObject {
+    @Published var error: Swift.Error?
+    
+    init(app: RealmSwift.App) {
+        app.syncManager.errorHandler = { syncError, syncSession in
+            self.error = syncError
         }
     }
 }
